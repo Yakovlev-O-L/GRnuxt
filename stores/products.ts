e34f822fbb5546346d2545e6594de316b55e2c72
Yakovlev-O-L <oleg81_404@mail.ru>
2024-12-03 20:42:32 +0300

    03.12.2024HOME

--- stores/products.ts
+++ stores/products.ts
@@ -44,12 +44,12 @@ export const useProducts = defineStore('products', () => {
       form: 'rozhok', weight:'110гр', inbox: '24шт', price:60, compound: 'белки, жиры и углеводы',
       images:['img\clavica\Bednyi_Evrey_110-258x258.jpg.jpg'],},
 
-    {id:9, title:'Вафельный стаканчик "Берёзка"', type:'icecream',
+    {id:9, title:'Стакан вафельный "Берёзка"', type:'icecream',
       description:'Мороженое в вафельном стаканчике',
       form: 'vafelniy stakan', weight:'70гр', inbox: '27шт', price:30, compound: 'белки, жиры и углеводы',
       images:['img\clavica\Berezka_70-258x258.jpg.jpg'],},
 
-    {id:10, title:'Бумажный стаканчик "Лимон"', type:'icecream',
+    {id:10, title:'Стакан бумажный "Лимон"', type:'icecream',
       description:'Лимонный десерт в бумажном стаканчике',
       form: 'bumazhniy stakan', weight:'100гр', inbox: '27шт', price:30, compound: 'белки, жиры и углеводы',
       images:['img\clavica\Desert_Limon_80-258x258.jpg.jpg'],},
@@ -59,7 +59,7 @@ export const useProducts = defineStore('products', () => {
       form: 'eskimo', weight:'70гр', inbox: '?шт', price:40, compound: 'белки, жиры и углеводы',
       images:['img\clavica\Dvoynoy_Shokolad_70-258x258.jpg.jpg'],},
 
-    {id:12, title:'Вафельный стакан "Фисташка"', type:'icecream',
+    {id:12, title:'Стакан вафельный "Фисташка"', type:'icecream',
       description:'Фисташковое мороженое с брусничным топпингом в плоском вафельном стаканчике',
       form: 'vafelniy stakan', weight:'80гр', inbox: '30шт', price:20, compound: 'белки, жиры и углеводы',
       images:['img\clavica\fistashka-258x258.jpg.jpg'],},
@@ -69,7 +69,7 @@ export const useProducts = defineStore('products', () => {
       form: 'vafelniy stakan', weight:'70гр', inbox: '24шт', price:20, compound: 'белки, жиры и углеводы',
       images:['img\clavica\Fruktoleto_Arbuz_Kivi_70г-1-258x258.jpg.jpg'],},
 
-    {id:14, title:'Стакан "Фруктолето" Лесные ягоды', type:'icecream',
+    {id:14, title:'Стакан бумажный "Фруктолето" Лесные ягоды', type:'icecream',
       description:'Мороженое ЛЕСНЫЕ ЯГОДЫ в бумажный стаканчик',
       form: 'bumazhniy stakan', weight:'100гр', inbox: '?шт', price:20, compound: 'белки, жиры и углеводы',
       images:['img\clavica\Fruktoleto_Lesnye_Yagodi_100-258x258.jpg.jpg'],},
@@ -79,12 +79,12 @@ export const useProducts = defineStore('products', () => {
       form: 'rozhok', weight:'110гр', inbox: '24шт', price:60, compound: 'белки, жиры и углеводы',
       images:['img\clavica\Hohol_110-258x258.jpg.jpg'],},
 
-    {id:16, title:'Вафельный стакан "Карамелька"', type:'icecream',
+    {id:16, title:'Стакан вафельный "Карамелька"', type:'icecream',
       description:'Мороженое крем-брюле с соленой карамелью в плоском вафельном стаканчике',
       form: 'vafelniy stakan', weight:'80гр', inbox: '30шт', price:20, compound: 'белки, жиры и углеводы',
       images:['img\clavica\karamelka-258x258.jpg.jpg'],},
 
-    {id:17, title:'Вафельный стакан "Клубничка"', type:'icecream',
+    {id:17, title:'Стакан вафельный "Клубничка"', type:'icecream',
       description:'Мороженое с клубничным топпингом в плоском вафельном стаканчике',
       form: 'vafelniy stakan', weight:'80гр', inbox: '30шт', price:20, compound: 'белки, жиры и углеводы',
       images:['img\clavica\klubnichka-258x258.jpg.jpg'],},
@@ -114,12 +114,12 @@ export const useProducts = defineStore('products', () => {
       form: 'eskimo', weight:'55гр', inbox: '24шт', price:40, compound: 'белки, жиры и углеводы',
       images:['img\clavica\Mark_Kivi_Kivi_55-258x258.jpg.jpg'],},
 
-    {id:23, title:'Стакан "Настроение" Чернослив', type:'icecream',
+    {id:23, title:'Стакан вафельный "Настроение" Чернослив', type:'icecream',
       description:'Мороженое с черносливовым джемом в большом вафельном стаканчике',
       form: 'vafelniy stakan', weight:'120гр', inbox: '20шт', price:30, compound: 'белки, жиры и углеводы',
       images:['img\clavica\nastroenie_120_chernosliv-258x258.jpg.jpg'],},
 
-    {id:24, title:'Стакан "Настроение" Клубника', type:'icecream',
+    {id:24, title:'Стакан вафельный "Настроение" Клубника', type:'icecream',
       description:'Мороженое с клубничным джемом в большом вафельном стаканчике',
       form: 'vafelniy stakan', weight:'120гр', inbox: '20шт', price:30, compound: 'белки, жиры и углеводы',
       images:['img\clavica\nastroenie_120_klubnika-258x258.jpg.jpg'],},
@@ -184,7 +184,7 @@ export const useProducts = defineStore('products', () => {
       form: 'eskimo', weight:'60гр', inbox: '24шт', price:30, compound: 'белки, жиры и углеводы',
       images:['img\clavica\Panda_60-258x258.jpg.jpg'],},
 
-    {id:37, title:'Бумажный стакан "Пломбир на Сливках"', type:'icecream',
+    {id:37, title:'Стакан бумажный "Пломбир на Сливках"', type:'icecream',
       description:'Пломбир в бумажном стаканчике',
       form: 'bumazhniy stakan', weight:'80гр', inbox: '27шт', price:20, compound: 'белки, жиры и углеводы',
       images:['img\clavica\Plombir_Na_Slivkah_80-1-258x258.jpg.jpg'],},
@@ -205,9 +205,104 @@ export const useProducts = defineStore('products', () => {
       images:['img\clavica\Plombir_Na_Slivkah_Rojok_100-258x258.jpg.jpg'],},
 
     {id:41, title:'Рожок "Пломбтр на сливках" шоколадный', type:'icecream',
-      description:'Шоколадный пломбир в вафельном сахарном рожке с шоколадной глазурь',
+      description:'Шоколадный пломбир в вафельном сахарном рожке с шоколадной глазурью',
       form: 'rozhok', weight:'100гр', inbox: '20шт', price:50, compound: 'белки, жиры и углеводы',
       images:['img\clavica\Plombir_Na_Slivkah_Rojok_shok_100_-258x258.jpg.jpg'],},
+
+    {id:42, title:'Эскимо "Пломбир на сливках" шоколадный', type:'icecream',
+      description:'Шоколадный пломбир на палочке в пергаменте',
+      form: 'eskimo', weight:'80гр', inbox: '28шт', price:30, compound: 'белки, жиры и углеводы',
+      images:['img\clavica\Plombir_Na_Slivkah_Shokoladniy_80-258x258.jpg.jpg'],},
+
+    {id:43, title:'Бумажный пакет "Пломбир на сливках"', type:'icecream',
+      description:'Пломбир в бумажном пакете',
+      form: 'bumazhniy paket', weight:'500гр', inbox: '12шт', price:250, compound: 'белки, жиры и углеводы',
+      images:['img\clavica\PLOMBIR-NA-SLIVKAKh_bum_paket500-258x258.jpg.jpg'],},
+
+    {id:44, title:'Ведро "Пломбир на сливках"', type:'icecream',
+      description:'Пломбир в бумажном ведерке',
+      form: 'bumazhniy paket', weight:'500гр', inbox: '6шт', price:300, compound: 'белки, жиры и углеводы',
+      images:['img\clavica\PLOMBIR-NA-SLIVKAKh_vedro-258x258.png.png'],},
+
+    {id:45, title:'Брикет "Пломбир на сливках"', type:'icecream',
+      description:'Пломбир в большом брикете',
+      form: 'briket', weight:'250гр', inbox: '17шт', price:150, compound: 'белки, жиры и углеводы',
+      images:['img\clavica\PNS-briket-258x258.png.png'],},
+
+    {id:46, title:'Брикет "Пломбир на сливках" клубничный', type:'icecream',
+      description:'Пломбир с клубничным джемом в большом брикете',
+      form: 'briket', weight:'250гр', inbox: '17шт', price:150, compound: 'белки, жиры и углеводы',
+      images:['img\clavica\PNS-briket-klub-258x258.png.png'],},
+
+    {id:47, title:'Брикет "Пломбир на сливках" шоколадный', type:'icecream',
+      description:'Шоколадный пломбир в большом брикете',
+      form: 'briket', weight:'250гр', inbox: '17шт', price:150, compound: 'белки, жиры и углеводы',
+      images:['img\clavica\PNS-briket-shok-258x258.png.png'],},
+
+    {id:48, title:'Стакан вафельный "Полюс" черника', type:'icecream',
+      description:'Сливочное мороженое с черничным джемом в вафельном стаканчике',
+      form: 'vafelniy stakan', weight:'80гр', inbox: '27шт', price:20, compound: 'белки, жиры и углеводы',
+      images:['img\clavica\Polyus_Chernika_80-258x258.jpg.jpg'],},
+
+    {id:49, title:'Пластиковое ведро "Полюс" клубника', type:'icecream',
+      description:'Сливочное мороженое с клубничным джемом в пластиковом ведерке',
+      form: 'plastikovoe vedro', weight:'500гр', inbox: '5шт', price:350, compound: 'белки, жиры и углеводы',
+      images:['img\clavica\POLYuS_plastikovoye_vedro_klubnika-258x258.jpg.jpg'],},
+
+    {id:50, title:'Стакан вафельный "Полюс" пьяная вишня', type:'icecream',
+      description:'Мороженое с вишнёвым джемом в вафельном стаканчике',
+      form: 'vafelniy stakan', weight:'80гр', inbox: '27шт', price:20, compound: 'белки, жиры и углеводы',
+      images:['img\clavica\Polyus_Pyanaya_Vishnya_80-258x258.jpg.jpg'],},
+
+    {id:51, title:'Пластиковый бокал "Полюс" черрника', type:'icecream',
+      description:'Сливочное мороженое с черничным джемом в пластиковом бокале',
+      form: 'plastikoviy bokal', weight:'280гр', inbox: '8шт', price:150, compound: 'белки, жиры и углеводы',
+      images:['img\clavica\Polyus_S_Chernichnim_Djemom_280-1-258x258.jpg.jpg'],},
+
+    {id:52, title:'Стакан вафельный "Полюс" шоколадная крошка', type:'icecream',
+      description:'Сливочное мороженое с шоколадной крошкой в вафельном стаканчике',
+      form: 'vafelniy stakan', weight:'80гр', inbox: '27шт', price:20, compound: 'белки, жиры и углеводы',
+      images:['img\clavica\Polyus_S_Shokoladom_80-258x258.jpg.jpg'],},
+
+    {id:53, title:'Пластиковый бокал "Полюс" варёная сгущенка', type:'icecream',
+      description:'Сливочное мороженое с вареным сгущенным молоком в пластиковом бокале',
+      form: 'plastikoviy bokal', weight:'280гр', inbox: '8шт', price:150, compound: 'белки, жиры и углеводы',
+      images:['public\img\clavica\Polyus_S_Varyonnoiy_Sguschenkoiy_280-1-258x258.jpg.jpg'],},
+
+    {id:54, title:'Стакан вафельный "Полюс" сгущёнка', type:'icecream',
+      description:'Сливочное мороженое с вареной сгущенкой в вафельном стаканчике',
+      form: 'vafelniy stakan', weight:'80гр', inbox: '27шт', price:20, compound: 'белки, жиры и углеводы',
+      images:['img\clavica\Polyus_So_Sguschonkoiy_80-258x258.jpg.jpg'],},
+
+    {id:55, title:'Стакан вафельный "Полюс" трюфель', type:'icecream',
+      description:'Шоколадное мороженое с шоколадным топпингом в вафельном стаканчике',
+      form: 'vafelniy stakan', weight:'80гр', inbox: '27шт', price:20, compound: 'белки, жиры и углеводы',
+      images:['img\clavica\Polyus_Tryufel_80-258x258.jpg.jpg'],},
+
+    {id:56, title:'Рожок "Рекорд"', type:'icecream',
+      description:'Двухслойное малиново-черничное мороженое в большом сахарном рожке',
+      form: 'rozhok', weight:'180гр', inbox: '12шт', price:70, compound: 'белки, жиры и углеводы',
+      images:['img\clavica\Rekord_180-258x258.jpg.jpg'],},
+
+    {id:57, title:'Торт "С любовью"', type:'icecream',
+      description:'Мороженое со сгущенкой и цукатами в пластиковой упаковке в виде сердца',
+      form: 'tort', weight:'500гр', inbox: '4шт', price:70, compound: 'белки, жиры и углеводы',
+      images:['img\clavica\S-LYuBOVYu-258x258.jpg.jpg'],},
+
+    {id:58, title:'Эскимо "Сердечко"', type:'icecream',
+      description:'Клубничное мороженое на палочке в форме сердечка',
+      form: 'eskimo', weight:'60гр', inbox: '20шт', price:20, compound: 'белки, жиры и углеводы',
+      images:['img\clavica\Serdechko_60-258x258.jpg.jpg'],},
+
+    {id:59, title:'Стакан вафельный "Шоколадка"', type:'icecream',
+      description:'Мороженое в вафельном стаканчике',
+      form: 'vafelniy stakan', weight:'70гр', inbox: '27шт', price:20, compound: 'белки, жиры и углеводы',
+      images:['img\clavica\Shokoladka_70-258x258.jpg.jpg'],},
+
+    {id:60, title:'Эскимо "Сырное"', type:'icecream',
+      description:'Сырное мороженое на палочке',
+      form: 'eskimo', weight:'65гр', inbox: '20шт', price:20, compound: 'белки, жиры и углеводы',
+      images:['img\clavica\Sirnoe_65-258x258.jpg.jpg'],},
   ])
 
   return { arr }
