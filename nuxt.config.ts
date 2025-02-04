19ba767d1c1108fadd6a4705213549ba9e16dbec
OlegYakovlev <oleg81_404@mail.ru>
2025-02-04 21:28:01 +0300

    5

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxt/image', 'shadcn-nuxt', '@nuxtjs/tailwindcss', '@pinia/nuxt'],
+  modules: ['@nuxt/image', 'shadcn-nuxt', '@nuxtjs/tailwindcss', '@pinia/nuxt','@prisma/nuxt'],
   shadcn: {
     /**
      * Prefix for all the imported component
@@ -13,5 +13,12 @@ export default defineNuxtConfig({
      * @default "./components/ui"
      */
     componentDir: './components/ui'
+  },
+  prisma: {
+    installCLI: true,
+    installClient: true,
+    generateClient: true,
+    installStudio: false,
+    autoSetupPrisma: true
   },
 })
